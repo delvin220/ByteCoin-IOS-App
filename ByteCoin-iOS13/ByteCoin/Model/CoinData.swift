533c75a2ea6e0e55eabbea6162d22fde17591d20
Delvin <70066996+delvin220@users.noreply.github.com>
2024-09-16 11:19:01 -0400

    Add files via upload

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by delvin han on 2024-08-26.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double 
+}
